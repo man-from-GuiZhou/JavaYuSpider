ad568670e0a341ac9548501427c3fc3c403b5614
yuhao <yuhao@YUHAO-PC>
2017-08-11 15:19:14 +0800

    仅仅爬取了百度首页页面信息

--- src/com/spider/main/SpiderMain.java
+++ src/com/spider/main/SpiderMain.java
@@ -1,10 +1,18 @@
 package com.spider.main;
 
+import com.spider.funcFactoryInte.VirtualFactory;
+import com.spider.funcInte.SpiderGet;
+import com.spider.othertools.XMLUtil;
+
 public class SpiderMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		VirtualFactory factory;
+		SpiderGet spiderGet;
+		factory = (VirtualFactory) XMLUtil.getBean();
+		spiderGet = factory.getSpiderWay();
+		spiderGet.getAllPage("https://www.baidu.com/");
 	}
 
 }
